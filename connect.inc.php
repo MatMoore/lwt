53fedfdc8e5028fe3e3ac9452f6635ac14ab9e90
Mat Moore <matmoore@users.noreply.github.com>
2022-06-26 11:46:38 +0100

    Environment based configuration to enable heroku deployment
    - Allow configuration via environment variables
    - Add .env file for docker-compose configuration
    - This removes the hardcoding in connect.inc.php

---
+++ connect.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * DB variables for XAMPP.
+ *
+ * @author https://sourceforge.net/projects/lwt/ LWT Project
+ * @since  1.0.3
+ */
+
+$server = $_ENV['DB_HOSTNAME'];
+$userid = $_ENV['DB_USER'];
+$passwd = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_DATABASE'];
+
+?>
